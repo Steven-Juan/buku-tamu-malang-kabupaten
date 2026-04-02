16e05827186e46b1eb8af21998554ea48646c496
Steven-Juan <stevjuan00@gmail.com>
2026-04-02 11:19:10 +0700

    upd data perangkat daerah fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,25 +43,35 @@ class DatabaseSeeder extends Seeder
                 // Buat 10 data per PD sesuai permintaan
                 for ($i = 1; $i <= 10; $i++) {
 
-                    // Logika Tanggal: Antara 27 Maret hingga 30 Maret 2026
-                    $hariAcak = rand(27, 30);
-
-                    // Logika Jam: Antara jam 08:00 pagi (8) hingga 16:00 sore (16)
+                    $hariAcak = rand(1, 6);
                     $jamAcak = rand(8, 16);
                     $menitAcak = rand(0, 59);
 
-                    $timestamp = \Carbon\Carbon::create(2026, 3, $hariAcak, $jamAcak, $menitAcak);
+                    $timestamp = \Carbon\Carbon::create(2026, 4, $hariAcak, $jamAcak, $menitAcak);
+
+                    // Definisi daftar avatar yang tersedia
+                    $daftarAvatar = [
+                        'scholar-man.png',
+                        'scholar-woman.png',
+                        'officer-man.png',
+                        'officer-woman.png',
+                        'guest-man.png',
+                        'guest-woman.png'
+                    ];
 
                     Guest::create([
                         'perangkat_daerah_id' => $pd->id,
-                        'nama' => $daftarNama[array_rand($daftarNama)],
-                        'asal_instansi' => $daftarUniversitas[array_rand($daftarUniversitas)],
-                        'keperluan' => 'Koordinasi teknis data ke-'.$i.' di '.$pd->nama_pd,
-                        'pesan_kesan' => 'Pelayanan cepat dan informatif.',
-                        'ttd_digital' => 'data:image/png;base64,sample_seed_'.$pd->id.'_'.$i,
-                        'foto' => 'images/user.png',
-                        'created_at' => $timestamp,
-                        'updated_at' => $timestamp,
+                        'nama'               => $daftarNama[array_rand($daftarNama)],
+                        'asal_instansi'      => $daftarUniversitas[array_rand($daftarUniversitas)],
+                        'keperluan'          => 'Koordinasi teknis data ke-' . $i . ' di ' . $pd->nama_pd,
+                        'pesan_kesan'        => 'Pelayanan cepat dan informatif.',
+                        'ttd_digital'        => 'data:image/png;base64,sample_seed_' . $pd->id . '_' . $i,
+
+                        // Memilih avatar secara acak dari folder images/avatars
+                        'foto'               => 'images/avatars/' . $daftarAvatar[array_rand($daftarAvatar)],
+
+                        'created_at'         => $timestamp,
+                        'updated_at'         => $timestamp,
                     ]);
                 }
             }
